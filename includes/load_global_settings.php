b0cb9fbd61ba6dc4f963dc581f72720a4ab9ce17
Drew Hamilton <drew.hamilton.avl@gmail.com>
2026-07-05 14:02:37 -0400

    Restore configured ITFlow theme selection

--- includes/load_global_settings.php
+++ includes/load_global_settings.php
@@ -131,7 +131,7 @@ $config_date_format = "M d, Y";
 $config_time_format = "g:i A";
 
 // Theme
-$config_theme = 'teal';
+$config_theme = $row['config_theme'];
 
 // Telemetry
 $config_telemetry = intval($row['config_telemetry']);
